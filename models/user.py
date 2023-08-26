85b08a8d73768989c7e182028fe5b2b60d1d1cbe
howard1357997531 <howard1357997531@yahoo.com.tw>
2023-08-26 14:04:34 +0800

    3

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ class Users(Base):
     __tablename__ = 'users'
 
     id = Column(String, primary_key=True)#設定它為主鍵
-    nickname_name = Column(String)#unique=True代表是不可以重複的
+    nick_name = Column(String)#unique=True代表是不可以重複的
     image_url= Column(String(length=256))#用戶的line名稱
     created_time = Column(DateTime, default=func.now())#大頭貼的url
 
